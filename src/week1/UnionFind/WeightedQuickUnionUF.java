c4b8b424e3d342a486d5107b5b6ef13d6d90e407
tsu-nera <fox10225fox@gmail.com>
2013-09-06 16:03:45 +0900

    add count i/f

--- src/week1/UnionFind/WeightedQuickUnionUF.java
+++ src/week1/UnionFind/WeightedQuickUnionUF.java
@@ -3,9 +3,11 @@ package week1.UnionFind;
 public class WeightedQuickUnionUF {
   private int[] id;
   private int[] sz;
+  private int count;
 
   public WeightedQuickUnionUF(int N)
   {
+    count = N;
     id = new int[N];
     sz = new int[N];
     for(int i = 0; i < N; i++) {
@@ -23,10 +25,14 @@ public class WeightedQuickUnionUF {
     }
     return i;
   }
+  
+  // Return the number of disjoint sets.
+  public int count() {
+      return count;
+  }
 
   public boolean connected(int p, int q)
   {
-
     return root(p) == root(q);
   }
 
@@ -34,12 +40,15 @@ public class WeightedQuickUnionUF {
   {
     int i = root(p);
     int j = root(q);
+    if (i == j) return;
+    
     if( sz[i] < sz[j]) {
       id[i] = j; sz[j] += sz[i];
     }
     else {
       id[j] = i; sz[i] += sz[j];
     }
+    count--;
   }
   
   public void showId()
