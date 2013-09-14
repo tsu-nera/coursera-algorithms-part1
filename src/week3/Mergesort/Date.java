d55f3810650ad4d2bed6e0debac6417a82b16bff
tsu-nera <fox10225fox@gmail.com>
2013-09-14 19:54:37 +0900

    date

---
+++ src/week3/Mergesort/Date.java
@@ -0,0 +1,26 @@
+package week3.Mergesort;
+
+
+public class Date implements Comparable<Date> 
+{
+  private final int month, day, year;
+  
+  public Date(int m, int d, int y)
+  {
+    month = m;
+    day   = d;
+    year  = y;
+  }
+
+  @Override
+  public int compareTo(Date that) {
+    if(this.year < that.year) return -1;
+    if(this.year > that.year) return +1;
+    if(this.month < that.month) return -1;
+    if(this.month > that.month) return +1;
+    if(this.day < that.day) return -1;
+    if(this.day > that.day) return +1;
+    return 0;
+  }
+  
+}
