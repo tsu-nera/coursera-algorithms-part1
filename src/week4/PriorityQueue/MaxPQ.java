cf21a820d753c9a5098d5ccaa8d4e83746562591
tsu-nera <fox10225fox@gmail.com>
2013-09-18 22:18:17 +0900

    MaxPQ

---
+++ src/week4/PriorityQueue/MaxPQ.java
@@ -0,0 +1,59 @@
+package week4.PriorityQueue;
+
+public class MaxPQ<Key extends Comparable<Key>>
+{
+  private Key[] pq;
+  private int N;
+  
+  public MaxPQ(int capacity)
+  { pq = (Key[]) new Comparable[capacity+1]; }
+  
+  public boolean isEmpty()
+  { return N == 0; }
+  
+  public void insert(Key x)
+  {
+    pq[++N] = x;
+    swim(N);
+  }
+  
+  public Key delMax()
+  {
+    Key max = pq[1];
+    exch(1, N--);
+    sink(1);
+    pq[N+1] = null;
+    return max;
+  }
+  
+  public void swim(int k)
+  {
+    while (k > 1 && less(k/2, k))
+    {
+      exch(k, k/2);
+      k = k/2;
+    }
+  }
+  
+  private void sink(int k)
+  {
+    while(2*k <= N)
+    {
+      int j = 2*k;
+      if (j < N && less(j, j+1)) j++;
+      if (!less(k, j)) break;
+      exch(k, j);
+      k = j;
+    }
+  }
+  
+  private boolean less(int i, int j)
+  { return pq[i].compareTo(pq[i]) < 0; }
+  
+  private void exch(int i, int j)
+  { 
+    Key t = pq[i];
+    pq[i] = pq[j];
+    pq[j] = t; 
+  }
+}
--- src/week4/PriorityQueue/UnorderdMaxPQ.java
+++ src/week4/PriorityQueue/UnorderdMaxPQ.java
@@ -19,20 +19,18 @@ public class UnorderdMaxPQ<Key extends Comparable<Key>>
     int max = 0;
     for (int i = 1; i < N; i++)
       if (less(max, i)) max = i;
-    exch(pq, max, N-1);
+    exch(max, N-1);
     return pq[--N];
   }
   
-  private static boolean less(Comparable v, Comparable w)
-  {
-    return (v.compareTo(w) < 0); 
-  }
+  private boolean less(int i, int j)
+  { return pq[i].compareTo(pq[i]) < 0; }
   
-  private static void exch(Comparable[] a, int i, int j)
-  {
-    Comparable swap = a[i];
-    a[i] = a[j];
-    a[j] = swap;
+  private void exch(int i, int j)
+  { 
+    Key t = pq[i];
+    pq[i] = pq[j];
+    pq[j] = t; 
   }
   
 }
